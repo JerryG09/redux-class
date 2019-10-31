aae04d813b80a006a6fde33de8ffed673d92dc2e
JerryG09 <phemmy09@gmail.com>
2019-10-31 10:27:52 +0100

    tooling

---
+++ src/store/configureStore.js
@@ -0,0 +1,14 @@
+import createLogger from "redux-logger";
+import { createStore, applyMiddleware} from "redux"
+import rootReducer from "./reducers"
+
+
+const loggerMiddleware = createLogger()
+const store = createStore(
+    rootReducer,
+    applyMiddleware(
+        loggerMiddleware
+    )
+)
+
+export default store
